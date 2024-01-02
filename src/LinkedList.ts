e682d69546fa4b8032cf611334f1c2a1011254da
Wayne Fuchs <104172375+waynefuchs@users.noreply.github.com>
2024-01-02 14:47:50 -0500

    Implement the `get(index)` method

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -5,49 +5,100 @@ type LinkedListNodeProp = {
 };
 
 export default class LinkedList {
-  isDoublyLinked;
-  isCircular;
-  first: LinkedListNodeProp | null;
-  last: LinkedListNodeProp | null;
-  size: number;
+  #isDoublyLinked;
+  #isCircular;
+  #first: LinkedListNodeProp | null;
+  #last: LinkedListNodeProp | null;
+  #size: number;
   constructor(isDoublyLinked = false, isCircular = false) {
-    this.isCircular = isCircular;
-    this.isDoublyLinked = isDoublyLinked;
-    this.first = null;
-    this.last = null;
-    this.size = 0;
+    this.#isCircular = isCircular;
+    this.#isDoublyLinked = isDoublyLinked;
+    this.#first = null;
+    this.#last = null;
+    this.#size = 0;
   }
 
+  /**
+   * Private method to create a linked list node
+   * @param data Data to place in the node
+   * @returns A LinkedListNode object with null links
+   */
   #createNode(data): LinkedListNodeProp {
     return {
       data,
       next: null,
-      ...(this.isDoublyLinked ? { prev: null } : {}),
+      ...(this.#isDoublyLinked ? { prev: null } : {}),
     };
   }
 
-  getFirst() {
-    return this.first;
+  /**
+   * Get the first node in the linked list
+   * @returns The first node in the linked list, null if there are no nodes in the linked list
+   */
+  getFirst(): LinkedListNodeProp | null {
+    return this.#first;
   }
-  // getEnd() {
-  //   return this.list.length <= 0 ? null : this.list[this.list.length - 1];
-  // }
+
+  /**
+   * Get the last node in the linked list
+   * @returns The last node in the linked list, null if there are no nodes in the linked list
+   */
+  getLast() {
+    return this.#last;
+  }
+
+  /**
+   * Get the number of elements in the linked list
+   * @returns A number representing the number of nodes in the linked list
+   */
   getSize(): number {
-    return this.size;
+    return Number(this.#size);
   }
+
+  /**
+   * Create a new node containing data and place it at the end of the linked list
+   * @param data Data to store in the new node
+   * @returns The newly created node
+   */
   append(data): LinkedListNodeProp {
+    // Create the node
     const newNode = this.#createNode(data);
-    if (this.last === null) {
-      this.first = this.last = newNode;
+
+    if (this.#last === null || this.#size === 0 || this.#first === null) {
+      // New node is the first node
+      this.#first = this.#last = newNode;
     } else {
-      this.last.next = newNode;
+      // Let the last node to be the newly created node and set links
+      const staleLast = this.#last;
+      staleLast.next = newNode;
+      this.#last = newNode;
     }
-    this.size++;
+
+    // Handle circular exception
+    if (this.#isCircular) {
+      this.#last.next = this.#first;
+      if (this.#isDoublyLinked) {
+        this.#first.prev = this.#last;
+      }
+    }
+
+    // Increment linked list size and return the newly created node
+    this.#size++;
     return newNode;
   }
-  // get(index: number) {
-  //   return index < this.getLength() ? this.list[index] : null;
-  // }
+
+  /**
+   * Iterate through the list and return the node at the supplied index
+   * @param index Numerical index in linked list
+   * @returns The node at the index if that index exists, otherwise null
+   */
+  get(index: number): LinkedListNodeProp | null {
+    if (index < 0 || index > this.#size || this.#first === null) return null;
+    let node: LinkedListNodeProp | null = this.#first;
+    for (let x = 1; x <= index; x++) node = node?.next || null;
+    return node;
+  }
+
   // #isLastIndex(index: number) {
   //   return index === this.getLength() - 1;
   // }
--- src/tests/LinkedList.test.ts
+++ src/tests/LinkedList.test.ts
@@ -9,12 +9,40 @@ describe("Linked List", () => {
       expect(newNode.data).toBe("hello");
       expect(ll.getFirst()?.data).toBe("hello");
     });
-    // it("Append should maintain correct order", () => {
-    //   const ll = new LinkedList();
-    //   const newNodeA = ll.append("hello");
-    //   const newNodeB = ll.append("world");
-    //   console.log(ll);
-    //   expect(ll.start.da);
-    // });
+    it("Append should maintain correct order", () => {
+      const ll = new LinkedList();
+      const newNodeA = ll.append("hello");
+      const newNodeB = ll.append("there");
+      const newNodeC = ll.append("world");
+      // Check first node
+      expect(ll.getFirst()?.data).toBe("hello");
+      expect(newNodeA.data).toBe("hello");
+      // Check second node
+      expect(newNodeA.next?.data).toBe("there");
+      expect(newNodeB.data).toBe("there");
+      //Check third / last node
+      expect(newNodeB.next?.data).toBe(ll.getLast()?.data);
+      expect(ll.getLast()?.data).toBe("world");
+
+      console.log(JSON.stringify(ll.getFirst()));
+    });
+    it("Returns the proper node for a given index", () => {
+      // create a new linked list
+      const ll = new LinkedList();
+      // with 9 nodes
+      const nodes = [
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqr",
+        "stu",
+        "vwx",
+        "yz",
+      ].map((v) => ll.append(v));
+      // check every returned data value against the linked list get(index) data value
+      nodes.every((v, i) => expect(v.data).toBe(ll.get(i)?.data));
+    });
   });
 });
