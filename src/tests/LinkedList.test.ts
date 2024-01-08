2977500aef6ee00885cad64a09d376a58360be25
Wayne Fuchs <104172375+waynefuchs@users.noreply.github.com>
2024-01-08 18:12:12 -0500

    Organize tests into describe groups

--- src/tests/LinkedList.test.ts
+++ src/tests/LinkedList.test.ts
@@ -1,7 +1,7 @@
 import LinkedList from "../LinkedList";
 
 describe("Linked List", () => {
-  describe("Push", () => {
+  describe("Push and Pop", () => {
     it("Should push a single item correctly", () => {
       const ll = new LinkedList();
       const newNode = ll.push("hello");
@@ -24,83 +24,6 @@ describe("Linked List", () => {
       //Check third / last node
       expect(newNodeB.next?.value).toBe(ll.tail?.value);
       expect(ll.tail?.value).toBe("world");
-    });
-
-    it("Returns the proper node for a given index", () => {
-      // create a new linked list
-      const ll = new LinkedList();
-      // with 9 nodes
-      const nodes = [
-        "abc",
-        "def",
-        "ghi",
-        "jkl",
-        "mno",
-        "pqr",
-        "stu",
-        "vwx",
-        "yz",
-      ].map((v) => ll.push(v));
-      // check every returned data value against the linked list get(index) data value
-      nodes.every((v, i) => expect(v.value).toBe(ll.get(i)?.value));
-      expect(ll.length).toBe(nodes.length);
-    });
-
-    it("Prepends correctly with unshift", () => {
-      const ll = new LinkedList({ doublyLinked: true });
-      const nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        .map((v) => ll.unshift(v))
-        .reverse();
-      nodes.every((v, i) => expect(v.value).toBe(ll.get(i)?.value));
-      expect(ll.length).toBe(nodes.length);
-    });
-
-    it("Insert will throw an error when corrupt link data is encountered", () => {
-      const ll = new LinkedList();
-      ll.push("one");
-      ll.push("two");
-      ll.push("three");
-      // break a link
-      !!ll && !!ll.head && (ll.head.next = null);
-      expect(() => {
-        ll.insert(2, "NOPE");
-      }).toThrow(Error);
-    });
-
-    it("Will insert things in the correct location", () => {
-      const ll = new LinkedList();
-      expect(ll.insert(-1, "fail")).toBeNull();
-      expect(ll.insert(1, "fail")).toBeNull();
-      expect(ll.length).toBe(0);
-
-      // first insertion
-      expect(ll.insert(0, "first")?.value).toBe("first");
-      expect(ll.head?.value).toBe("first");
-      expect(ll.tail?.value).toBe("first");
-
-      // second
-      expect(ll.insert(1, "last")?.value).toBe("last");
-      expect(ll.head?.value).toBe("first");
-      expect(ll.tail?.value).toBe("last");
-      expect(ll.get(0)?.value).toBe("first");
-      expect(ll.get(1)?.value).toBe("last");
-
-      // let's put 100 insertions in the middle (reverse order)
-      const arr = [...new Array(100)].map((_, i) => i);
-      arr.forEach((v) => ll.insert(1, v));
-      expect(ll.get(1)?.value).toBe(99);
-      expect(ll.get(50)?.value).toBe(50);
-      expect(ll.get(100)?.value).toBe(0);
-
-      // Check length for funsies
-      expect(ll.length).toBe(102);
-    });
-
-    it("Will insert things in the correct location with doubly linked list", () => {
-      const ll = new LinkedList({ doublyLinked: true });
-      expect(ll.insert(0, "zero")?.value).toBe("zero");
-      expect(ll.insert(1, "two")?.value).toBe("two");
-      expect(ll.insert(1, "one")?.value).toBe("one");
     });
 
     it("Should pop no items correctly", () => {
@@ -143,6 +66,46 @@ describe("Linked List", () => {
       expect(() => {
         ll.pop();
       }).toThrow(Error);
+    });
+  });
+
+  describe("Get", () => {
+    it("Returns the proper node for a given index", () => {
+      // create a new linked list
+      const ll = new LinkedList();
+      // with 9 nodes
+      const nodes = [
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqr",
+        "stu",
+        "vwx",
+        "yz",
+      ].map((v) => ll.push(v));
+      // check every returned data value against the linked list get(index) data value
+      nodes.every((v, i) => expect(v.value).toBe(ll.get(i)?.value));
+      expect(ll.length).toBe(nodes.length);
+    });
+
+    it("Get with invalid index should return null", () => {
+      const ll = new LinkedList();
+      expect(ll.get(-1)).toBeNull();
+      expect(ll.get(1)).toBeNull();
+      expect(ll.get(0)).toBeNull();
+    });
+  });
+
+  describe("Shift and Unshift", () => {
+    it("Prepends correctly with unshift", () => {
+      const ll = new LinkedList({ doublyLinked: true });
+      const nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        .map((v) => ll.unshift(v))
+        .reverse();
+      nodes.every((v, i) => expect(v.value).toBe(ll.get(i)?.value));
+      expect(ll.length).toBe(nodes.length);
     });
 
     it("Shift should set prev link on head for more than one node (edge case)", () => {
@@ -195,12 +158,55 @@ describe("Linked List", () => {
       expect(ll.shift()?.value).toBe("Bravo");
       expect(ll.length).toBe(0);
     });
+  });
 
-    it("Get with invalid index should return null", () => {
+  describe("Insert", () => {
+    it("Insert will throw an error when corrupt link data is encountered", () => {
       const ll = new LinkedList();
-      expect(ll.get(-1)).toBeNull();
-      expect(ll.get(1)).toBeNull();
-      expect(ll.get(0)).toBeNull();
+      ll.push("one");
+      ll.push("two");
+      ll.push("three");
+      // break a link
+      !!ll && !!ll.head && (ll.head.next = null);
+      expect(() => {
+        ll.insert(2, "NOPE");
+      }).toThrow(Error);
+    });
+
+    it("Will insert things in the correct location", () => {
+      const ll = new LinkedList();
+      expect(ll.insert(-1, "fail")).toBeNull();
+      expect(ll.insert(1, "fail")).toBeNull();
+      expect(ll.length).toBe(0);
+
+      // first insertion
+      expect(ll.insert(0, "first")?.value).toBe("first");
+      expect(ll.head?.value).toBe("first");
+      expect(ll.tail?.value).toBe("first");
+
+      // second
+      expect(ll.insert(1, "last")?.value).toBe("last");
+      expect(ll.head?.value).toBe("first");
+      expect(ll.tail?.value).toBe("last");
+      expect(ll.get(0)?.value).toBe("first");
+      expect(ll.get(1)?.value).toBe("last");
+
+      // let's put 100 insertions in the middle (reverse order)
+      const arr = [...new Array(100)].map((_, i) => i);
+      arr.forEach((v) => ll.insert(1, v));
+      expect(ll.get(1)?.value).toBe(99);
+      expect(ll.get(50)?.value).toBe(50);
+      expect(ll.get(100)?.value).toBe(0);
+
+      // Check length for funsies
+      expect(ll.length).toBe(102);
+    });
+
+    it("Will insert things in the correct location with doubly linked list", () => {
+      const ll = new LinkedList({ doublyLinked: true });
+      expect(ll.insert(0, "zero")?.value).toBe("zero");
+      expect(ll.insert(1, "two")?.value).toBe("two");
+      expect(ll.insert(1, "one")?.value).toBe("one");
     });
   });
 });
