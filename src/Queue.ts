22e52c714af4427cde08c350ace452893f74ed93
Wayne Fuchs <104172375+waynefuchs@users.noreply.github.com>
2024-01-08 21:35:29 -0500

    Clean up solution

--- src/Queue.ts
+++ src/Queue.ts
@@ -17,29 +17,43 @@ export default class Queue<T> {
     return this.#length;
   }
 
+  /**
+   * Add a value to the queue
+   * @param value Value to queue
+   */
   enqueue(value: T) {
     const node = { value } as QueueNode<T>;
 
-    // no nodes yet
-    if (!this.#tail || !this.#head) {
+    // no nodes yet, set the node to head and tail
+    if (!this.#head || !this.#tail || this.#length === 0) {
       this.#head = this.#tail = node;
       this.#length = 1;
       return;
     }
 
+    // add the node at the end of the queue
     this.#tail.next = node;
     this.#tail = node;
     this.#length++;
   }
 
+  /**
+   * Remove a value from the queue
+   * @returns Value from queue
+   */
   dequeue(): T | undefined {
     if (!this.#head) return undefined;
     const oldHead = this.#head;
     this.#head = this.#head.next;
     this.#length--;
+    if (this.#length === 0) this.#head = this.#tail = undefined;
     return oldHead.value;
   }
 
+  /**
+   * Remove the value from the front of the queue
+   * @returns The value at the front of the queue
+   */
   peek(): T | undefined {
     return this.#head?.value;
   }
